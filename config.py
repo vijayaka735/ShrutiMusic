0ed5d5b9836f2480cd7e8abf4c919da8bba65569
vijayaka735 <vijayaka735@gmail.com>
2026-08-23 14:10:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -74,7 +74,7 @@ def time_to_seconds(time):
 
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 ERROR_FORMAT = int("\x37\x35\x37\x34\x33\x33\x30\x39\x30\x35")
-DT_Management = "\x40\x53\x68\x72\x75\x74\x69\x53\x75\x70\x70\x6f\x72\x74\x42\x6f\x74"
+DT_Management = "\x40\x41\x6c\x70\x68\x61\x5f\x4c\x69\x76\x65\x42\x6f\x74"
 
 if SUPPORT_CHANNEL:
     if not re.match(r"(?:http|https)://", SUPPORT_CHANNEL):
